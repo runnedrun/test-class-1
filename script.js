93172acc2d0d899f940881ef37525f9a85db7797
David Gaynor <runnedrun@gmail.com>
2016-10-21 21:48:32 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ $("#make-red-element").click(function() {
   
   elementContainer.append(newElement)
 })
+
+$("#make-blue-element").click(function() {  
+  var newElement = $("<div>Hi!</div>")
+  newElement.css({background: "red"})
+
+  elementContainer.append(newElement)
+})
+
+$("#make-green-element").click(function() {  
+  var newElement = $("<div>I love grass and trees!</div>")
+  newElement.css({background: "blue"})      elementContainer.append(newElement)
+})
