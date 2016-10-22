2da32622aba160ec426c23a1e850e1a8c1148d3e
David Gaynor <runnedrun@gmail.com>
2016-10-21 20:45:24 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ var elementContainer = $("#generated-element-container")
 
 $("#make-red-element").click(function() {  
   var newElement = $("<div>")
-  newElement
+  newElement.css({
+    color: "red"
+  })
+  
   elementContainer.append(
 })
+  
+function makeNewElement(){
+  var newElement = $("<div>")
+  newElement.css({
+    color: "red"
+  })
+  
+  return newElement
+}
